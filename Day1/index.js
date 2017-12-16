33b9fdd622147bb4f0e7832f0809b8e5176ecde1
Archawin <zepalzth@gmail.com>
2017-12-16 23:55:09 +0700

    Commit Day1

---
+++ Day1/index.js
@@ -0,0 +1,15 @@
+$(() => {
+    
+  $('.answer').on('click', () => {    
+    alert('Correct !!')
+  })
+
+  $('#trick').on('mouseover', () => {
+    $('#trick').css({
+      position: 'absolute',
+      top: Math.floor(Math.random() * 90) + '%',
+      left: Math.floor(Math.random() * 90) + '%'
+    })
+  })
+  
+})
