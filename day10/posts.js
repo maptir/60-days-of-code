2e4625953896316749046c330bf6b3da78afa15a
Archawin <zepalzth@gmail.com>
2017-12-29 04:22:28 +0700

    Add day10

---
+++ day10/posts.js
@@ -0,0 +1,25 @@
+const posts = [{
+  id: 0,
+  topic: 'TEST SANTIP TOPIC',
+  user: 'ZEpalZ',
+  likes: 123,
+  message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum odio asperiores, corrupti accusantium laborum tenetur rem consequuntur mollitia facere, eveniet voluptate itaque repudiandae pariatur veritatis, iusto quos quaerat eaque!',
+  comments: [{
+      user: 'KykungZ',
+      likes: 4,
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum odio asperiores, corrupti accusantium laborum tenetur rem consequuntur mollitia facere, eveniet voluptate itaque repudiandae pariatur veritatis, iusto quos quaerat eaque!'
+    },
+    {
+      user: 'Jamie',
+      likes: 98,
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum odio asperiores, corrupti accusantium laborum tenetur rem consequuntur mollitia facere, eveniet voluptate itaque repudiandae pariatur veritatis, iusto quos quaerat eaque!'
+    },
+    {
+      user: 'hereton',
+      likes: 1234,
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eum odio asperiores, corrupti accusantium laborum tenetur rem consequuntur mollitia facere, eveniet voluptate itaque repudiandae pariatur veritatis, iusto quos quaerat eaque!'
+    }
+  ]
+}]
+
+module.exports = posts
---
+++ day10/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const ip = require('ip');
+const bodyParser = require('body-parser');
+const posts = require('./posts');
+
+const port = 1975
+const app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+
+app.use(express.static(__dirname + '/publics'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded(() => {
+  extended: true
+}))
+
+app.get('/', (req, res) => {
+  const context = {
+    title: 'SANTIP',
+    header: 'Welcome to SANTIP',
+    message: `There is ${posts.length} topics`,
+    posts
+  }
+  res.render('home', context)
+})
+
+app.get('/createTopic', (req, res) => {
+  res.render('addTopic')
+})
+
+app.post('/addtopic', (req, res) => {
+  posts.push({
+    id: posts.length,
+    topic: req.body.topic,
+    user: req.body.user,
+    message: req.body.message,
+    likes: 0,
+    comments: []
+  })
+  res.redirect('/')
+})
+
+app.post('/addcomment', (req, res) => {
+  posts[req.body.id].comments.push({
+    user: req.body.user,
+    likes: 0,
+    message: req.body.message
+  })
+  res.redirect(`/posts/${req.body.id}`)
+})
+
+app.post('/addliketopic', (req, res) => {
+  posts[req.body.id].likes++
+  res.redirect(`/posts/${req.body.id}`)
+})
+
+app.post('/addlikecomment', (req, res) => {
+  posts[req.body.id].comments[req.body.commentid].likes++
+  res.redirect(`/posts/${req.body.id}`)
+})
+
+app.get('/posts/:id', (req, res, next) => {
+  const id = req.params.id
+  const post = posts[id]
+  if (post) res.render('post', post)
+  else next()
+})
+
+app.get('*', (req, res) => {
+  res.status(404).send('404 PAGE NOT FOUND')
+})
+
+app.listen(port, () => {
+  console.log(`Server is started on ${ip.address()} : ${port}`);
+})
